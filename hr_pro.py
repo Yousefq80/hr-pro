bd48dc14682e50e5e17c1a9182a9e27090220edb
Yousefq80 <ysalharby@gmail.com>
2022-10-02 20:14:11 +0300

    not final answer

--- hr_pro.py
+++ hr_pro.py
@@ -1,5 +1,4 @@
-from sys import builtin_module_names
-from unicodedata import name
+
 
 
 class Employee:
@@ -14,35 +13,35 @@ class Employee:
         return self.sal*12
     
     def showemp(self):
-        print(f"name:,{self.name}age:,{self.age}salary:,{self.sal}working year:,{self.doj}")
+        print(f"name:,{self.name}/age:,{self.age}/:salary:,{self.sal}/working year:,{self.doj}")
     def addemp(self):
         self.name=(input("Enter emp name:\t"))
         self.age = int(input("Enter age:\t"))
         self.sal = input("Enter salary:\t")
-        self.jod = input("Enter working years:\t")
+        self.doj = input("Enter working years:\t")
      
     def __str__(self):
         return f"name:,{self.name}age:,{self.age}salary:,{self.sal}working year:,{self.doj}"
     
     
-    ch = int(input("Enter your Choice "))  
+    
    #Employee class here
 
 class Manager(Employee):
-    def __init__(self, name, age, sal, doj,bonus_per):
+    def __init__(self, name, age, sal, doj):
         super().__init__(name, age, sal, doj)
-        self.bonus_per=bonus_per
-        total=self.sal*(self.bonus_per/100)
+        
+        
     #Manager class here
     def getbonus(self):
-        return self.sal * self.bonus
+        return self.sal * (20/100)
     def showmang(self):
         print(f"name:,{self.name}age:,{self.age}salary:,{self.sal}working year:,{self.doj}bonus:,{self.getbonus}")
     def addmng(self):
        self.name=(input("Enter emp name:\t"))
        self.age =int(input("Enter age:\t"))
        self.sal = input("Enter salary:\t")
-       self.jod = input("Enter working years:\t") 
+       self.doj = input("Enter working years:\t") 
     def __str__(self):
        return f"name:,{self.name}age:,{self.age}salary:,{self.sal}working year:,{self.doj}bonus:,{self.getbonus}"
     def menu(self):
@@ -50,47 +49,35 @@ class Manager(Employee):
         print("Press ")
         print("1 to show all emp list")
         print("2 to show  manager")
-        print("3 to Promote add emp")
+        print("3 to add emp")
         print("4 to add manager")
         print("5 to Exit")
         ch = int(input("Enter your Choice "))
         if ch == 1:
-         self.showemp
+         self.showemp()
         elif ch == 2:
-         self.showmang
+         self.showmang()
         elif ch == 3:
-         self.addemp
+         self.addemp()
         elif ch == 4:
-         self.addmng
+         self.addmng()
         elif ch == 5:
          exit(0)
         else:
           print("Invalid Choice")   
 def main():
 
-    # print("Welcome to Employee Management Record")
-    # print("Press ")
-    # print("1 to show all emp list")
-    # print("2 to show  manager")
-    # print("3 to Promote add emp")
-    # print("4 to add manager")
-    # print("5 to Exit")
-    # if ch == 1:
-    #     showemp()
-    # elif ch == 2:
-    #     Remove_Employ()
-    # elif ch == 3:
-    #     Promote_Employee()
-    # elif ch == 4:
-    #     Display_Employees()
-    # elif ch == 5:
-    #     exit(0)
-    # else:
-    #     print("Invalid Choice")
-    
-    # ch = int(input("Enter your Choice "))
+ emp1=Employee("yousef",37,2000,17)
+ emp2=Employee("naif",20,1000,12)
+ ma=Manager("Rashed",50,4000,25)
+ ma.menu()
 
-	# main code here
+#  emp1.addemp()
+#  emp2.addemp()
+#  ma.addmng()
  
+     
+     
+     
 if __name__ == '__main__':
 	main()
